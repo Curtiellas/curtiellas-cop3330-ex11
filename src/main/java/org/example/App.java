7c40a7b125c4eaa46a2cf85b6c0724c773398b53
Luis Curtiellas <curtiellas98@gmail.com>
2021-09-13 19:55:06 -0400

    re

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,15 +13,15 @@ public class App
     {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("How many euros are you exchanging?");
+        System.out.print("How many euros are you exchanging? ");
         float euros = sc.nextFloat();
 
-        System.out.println("What is the exchange rate?");
+        System.out.print("What is the exchange rate? ");
         float rate = sc.nextFloat();
 
         //calculate
         float dollars = euros * rate;
 
-        System.out.printf("%.0f euros at an exchange rate of %.4f is\n%.2f U.S. dollars.", euros, rate, dollars);
+        System.out.printf("%.2f euros at an exchange rate of %.4f is\n%.2f U.S. dollars.", euros, rate, dollars);
     }
 }
